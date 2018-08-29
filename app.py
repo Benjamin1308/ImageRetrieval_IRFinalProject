efcbfd0b27bea709dd2dee9330d0fc5879a03062
npkhoa2197 <npkhoa@apcs.vn>
2018-08-29 12:24:16 +0700

    update api

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, Response
 import requests
+import RetrieveImage
 
 app = Flask(__name__)
 
-def some_long_calculation(number):
-  '''
-  here will be some long calculation using this number
-  let's simulate that using sleep for now :)
-  '''
-  import time
-  time.sleep(5)
+# def some_long_calculation(number):
+#   '''
+#   here will be some long calculation using this number
+#   let's simulate that using sleep for now :)
+#   '''
+#   import time
+#   time.sleep(5)
 
-  return number
+#   return number
 
-@app.route('/')
-def check():
-    def generate():
-      for i in range(10):
-        yield "<br/>"   # notice that we are yielding something as soon as possible
-        yield str(some_long_calculation(i))
-    return Response(generate(), mimetype='text/html')
+@app.route('/images', methods=['POST'])
+def getImages():
+    if (request.method == 'POST'):
+      def generate():
+          yield "<br/>"   # notice that we are yielding something as soon as possible
+          yield RetrieveImage.retrieveImage(request.form['base64'])
+      return Response(generate(), mimetype='text/html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
