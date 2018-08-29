7bc7a213c890b3b04ca0918928ee458de2c28740
npkhoa2197 <npkhoa@apcs.vn>
2018-08-29 11:00:05 +0700

    another bypass

--- app.py
+++ app.py
@@ -10,13 +10,17 @@ class ImageList(Resource):
       parser = reqparse.RequestParser()
       parser.add_argument("base64")
       args = parser.parse_args()
-      yield "<br/>"
       images = retrieveImage(str(args["base64"]))
       if len(images) == 0:
         return "No matching result", 200
       return images, 200
+
+class ByPass(Resource):
+  def post(self):
+    return None, 200
       
 api.add_resource(ImageList, '/images')
+api.add_resource(ByPass, '/images')
 
 if __name__ == '__main__':
   app.run()
