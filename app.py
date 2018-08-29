03469165e03c1311312bc4c398f6d77cb359a5f9
npkhoa2197 <npkhoa@apcs.vn>
2018-08-29 11:58:40 +0700

    update api

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ class ImageList(Resource):
       parser.add_argument("base64")
       args = parser.parse_args()
       images = retrieveImage(str(args["base64"]))
-      if len(images) == 0:
-        return "No matching result", 200
       return images, 200
       
 api.add_resource(ImageList, '/images')
